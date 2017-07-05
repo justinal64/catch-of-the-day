1a474af2a25c6da2c9ec68c58b1b5592aba621f7
justin leggett <justin@codingrookie.com>
2017-07-05 15:55:43 -0500

    initial push

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from "re-base";
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyC42L2XIsDJ2bXjdGkfI5fYsZfiCP4HZ6I",
+  authDomain: "catch-of-the-day-jl-21880.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-jl-21880.firebaseio.com"
+});
+
+export default base;
---
+++ src/components/Fish.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { formatPrice } from "../helpers";
+
+class Fish extends React.Component {
+  render() {
+    // shortcut so we don;t have to type this.props over and over
+    const { details, index } = this.props;
+    const isAvailable = details.status === "available";
+    const buttonText = isAvailable ? "Add To Order" : "Sold Out!";
+    return (
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name} />
+        <h3 className="fish-name">
+          {details.name}
+          <span className="price">
+            {formatPrice(details.price)}
+          </span>
+        </h3>
+        <p>
+          {details.desc}
+        </p>
+        <button
+          disabled={!isAvailable}
+          onClick={() => this.props.addToOrder(index)}
+        >
+          {buttonText}
+        </button>
+      </li>
+    );
+  }
+}
+
+export default Fish;
